7fce148a412edb26ab1fe9683d497e2ce6d7ccd4
zachbil5 <55146215+zachbil5@users.noreply.github.com>
2020-10-01 15:57:17 -0400

    Player Movement
    Added Player movement with mouse and also projectiles.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject ProjectilePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos2D = Input.mousePosition; // 1
+                                                  // The Camera's z position sets the how far to push the mouse into 3D
+        mousePos2D.z = -Camera.main.transform.position.z; // 2
+                                                          // Convert the point from 2D screen space into 3D game world space
+        Vector3 mousePos3D =
+        Camera.main.ScreenToWorldPoint(mousePos2D); // 3
+                                                    // Move the x position of this Basket to the x position of the Mouse
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        pos.y = mousePos3D.y;
+        pos.x = Mathf.Clamp(pos.x,-19, 19);
+        pos.y = Mathf.Clamp(pos.y, 1, 19);
+        this.transform.position = Vector3.Lerp(transform.position,pos,0.05f);
+    }
+
+    private void OnMouseDown()
+    {
+       GameObject projectile = Instantiate(ProjectilePrefab) as GameObject;
+        projectile.transform.position = transform.position;
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private static float top = 20;
+    public float speed = 10f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y + speed * Time.deltaTime);
+       if(transform.position.y > top)
+        {
+            Destroy(this.gameObject);
+        } 
+    }
+}
